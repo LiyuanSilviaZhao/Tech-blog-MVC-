297013d9b01f375e2b0a011ac0d1bd7264de6a75
LiyuanSilviaZhao <zhao0301liyuan@gmail.com>
2022-12-18 20:12:30 -0800

    adding home, dashboard

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -3,40 +3,54 @@
 const express = require('express');
 const router = express.Router();
 const {User,Post,Comment} = require('../models');
+const moment = require('moment-timezone');
 
 router.get("/",(req,res)=>{
-    res.render("home",{
-            logged_in:req.session.logged_in
+
+    Post.findAll({
+        include:[{
+            model:User,
+            attributes:['username'],
+        }],
+
+    }).then(allpost=>{
+        const postsHbsData = allpost.map(post=>post.get({plain:true}))
+        const postsdata = postsHbsData.map(post=>post.date = moment(post.createdAt).format('MM/DD/YYYY'));
+        res.render("home", {
+            posts: postsHbsData,
         })
+
+    })
 })
 
 router.get("/login",(req,res)=>{
-    if(req.session.logged_in){
-        return res.redirect("/")
-    }
-    res.render("login")
-})
-
-router.get("/signup",(req,res)=>{
-    if(req.session.logged_in){
-        return res.redirect("/")
-    }
     res.render("signup")
 })
 
+router.get("/sessions",(req,res)=>{
+    res.json(req.session)
+})
 
-router.get("/profile",(req,res)=>{
+router.get("/dashboard",(req,res)=>{
     if(!req.session.logged_in){
         return res.redirect("/login")
     }
+
     User.findByPk(req.session.user_id,{
-        include:[Project]
+        include:[Post]
     }).then(userData=>{
         const hbsData = userData.toJSON();
         console.log(hbsData)
         hbsData.logged_in=req.session.logged_in
-        res.render("profile",hbsData)
+        res.render("dashboard",hbsData)
     })
+})
+
+router.get("/create-post",(req,res)=>{
+    if(!req.session.logged_in){
+        return res.redirect("/login")
+    }
+    res.render("createpost")
 })
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -2,10 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require("bcrypt")
 
-class User extends Model {}
+class User extends Model {
+        checkPassword(loginPw) {
+            return bcrypt.compareSync(loginPw, this.password);
+        }
+}
 
 User.init({
-    // add properites here, ex:
     username: {
          type: DataTypes.STRING,
          allowNull:false,
